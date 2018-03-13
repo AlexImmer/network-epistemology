ed5cf69a473e4ed5ca90c2b5838b02ff045b0f5c
Alexander Immer <alexndr.immer@gmail.com>
2018-03-13 16:31:31 +0100

    Add dedicated loading file

---
+++ loading.py
@@ -0,0 +1,43 @@
+import json
+import pickle
+import numpy as np
+
+
+corpus_files = ['publication_data/dblp-ref-0.json',
+                'publication_data/dblp-ref-1.json',
+                'publication_data/dblp-ref-2.json',
+                'publication_data/dblp-ref-3.json']
+
+
+def load_file(dblp_file):
+    ids, texts, dates = [], [], []
+    with open(dblp_file) as f:
+        for line in f:
+            line = json.loads(line)
+            text = line.get('title', '') + '. ' + line.get('abstract', '')
+            text = text.replace('- ', '')
+            ids.append(line['id'])
+            dates.append(line['year'])
+            texts.append(text)
+    return ids, texts, dates
+
+
+def load_corpus(files=None):
+    files = files or corpus_files
+    ids_corpus, text_corpus, dates_corpus = [], [], []
+    for file in files:
+        ids, corpus, dates = load_file(file)
+        ids_corpus.extend(ids)
+        text_corpus.extend(corpus)
+        dates_corpus.extend(dates)
+    return ids_corpus, text_corpus, dates_corpus
+
+
+def load_doc_topics(file=None):
+    file = file or 'data/X_multopic.npy'
+    return np.load(file)
+
+
+def load_min_dists(file=None):
+    file = file or 'data/min_dists.pkl'
+    return pickle.load(file)
