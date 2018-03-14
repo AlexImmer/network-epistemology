777fdd625c3db9e047eb3e41504292165ba0fd48
Alexander Immer <alexndr.immer@gmail.com>
2018-03-14 11:38:24 +0100

    Use document ids as defined by dblp

--- document_aliveness.py
+++ document_aliveness.py
@@ -1,35 +1,10 @@
 import numpy as np
 import pandas as pd
 import pickle
-from multiprocessing import Pool
 from scipy.spatial.distance import cdist
 from tqdm import tqdm
 
 from loading import load_corpus, load_doc_topics
-
-
-def obtain_min_distances_parallel(X_topic, years, max_mem=36000000):
-    res = {}
-    for year in tqdm(sorted(list(years))[1:]):
-        cols = np.arange(0, 100)
-        X_prev = X_topic[X_topic['year'] < year][cols]
-        X_cur = X_topic[X_topic['year'] == year][cols]
-        ix_stepsize = int(max_mem / len(X_cur))
-
-        def compute_cdist(start):
-            return cdist(X_prev.iloc[start:start+ix_stepsize].values, X_cur.values,
-                         metric='cityblock').min(axis=1)
-
-        iterable = range(0, len(X_prev), ix_stepsize)
-        with Pool(processes=4) as pool:
-            X_prevsubs = pool.map(compute_cdist, iterable)
-
-        min_dist = pd.DataFrame(index=X_prev.index)
-        for i, res in zip(range(0, len(X_prev), ix_stepsize), X_prevsubs):
-            min_dist.iloc[i: i+ix_stepsize] = res
-
-        res[year] = min_dist
-    return res
 
 
 def obtain_min_dist_split(X_topic, years, max_mem=10**9):
@@ -44,7 +19,7 @@ def obtain_min_dist_split(X_topic, years, max_mem=10**9):
         for i in tqdm(range(0, len(X_prev), ix_stepsize)):
             dists = cdist(X_prev.iloc[i: i+ix_stepsize].values, X_cur.values, metric='cityblock')
             min_dist.iloc[i: i+ix_stepsize, 0] = dists.min(axis=1)
-            min_dist.iloc[i: i+ix_stepsize, 1] = dists.argmin(axis=1)
+            min_dist.iloc[i: i+ix_stepsize, 1] = X_cur.index[dists.argmin(axis=1)]
 
         res[year] = min_dist
     return res
@@ -66,10 +41,10 @@ if __name__ == '__main__':
     # compute distance list following the scheme
     # list[year] : DataFrame.loc[pubs before year] : dist to closest pub of year in [0, 1]
     print('load corpus')
-    _, _, dates_corpus = load_corpus()
+    ids_corpus, _, dates_corpus = load_corpus()
     print('load doc topics')
     X_multopic = load_doc_topics()
-    X_multopic = pd.DataFrame(X_multopic)
+    X_multopic = pd.DataFrame(X_multopic, index=ids_corpus)
     X_multopic['year'] = dates_corpus
     print('compute distances')
     min_dists = obtain_min_dist_split(X_multopic, set(dates_corpus), max_mem=10**9)
