3387a105e5f3c6692e459663cc977f1e16a5c9dd
Alexander Immer <alexndr.immer@gmail.com>
2018-04-04 12:02:07 +0200

    Add document aliveness update

--- document_aliveness.py
+++ document_aliveness.py
@@ -3,26 +3,43 @@ import pandas as pd
 import pickle
 from scipy.spatial.distance import cdist
 from tqdm import tqdm
+from multiprocessing import Pool
 
 from loading import load_corpus, load_doc_topics
 
 
-def obtain_min_dist_split(X_topic, years, max_mem=10**9):
-    res = {}
+def obtain_min_dist_split(X_topic, years, max_mem=10**7):
     for year in tqdm(sorted(list(years))[1:]):
         cols = np.arange(0, 100)
-        X_prev = X_topic[X_topic['year'] < year][cols]
-        X_cur = X_topic[X_topic['year'] == year][cols]
+        X_prev = X_topic.loc[(X_topic['year'] < year), cols].values
+        global X_cur
+        X_cur = X_topic.loc[(X_topic['year'] == year), cols].values
         ix_stepsize = int(max_mem / len(X_cur))
-
         min_dist = pd.DataFrame(index=X_prev.index, columns=['min_dist', 'closest_doc'])
-        for i in tqdm(range(0, len(X_prev), ix_stepsize)):
-            dists = cdist(X_prev.iloc[i: i+ix_stepsize].values, X_cur.values, metric='cityblock')
-            min_dist.iloc[i: i+ix_stepsize, 0] = dists.min(axis=1)
-            min_dist.iloc[i: i+ix_stepsize, 1] = X_cur.index[dists.argmin(axis=1)]
+        # multiproc here
+        X_prevs = [X_prev[i: i+ix_stepsize] for i in range(0, len(X_prev), ix_stepsize)]
+        print('created x_prevs')
+        with Pool(processes=8) as pool:
+            res_list = pool.map(compute_dists, X_prevs)
+        print('finished pool')
+        dists = []
+        closest = []
+        for res in res_list:
+            dists.extend(res[0])
+            closest.extend(res[1])
+        print('finished list extensions')
+        min_dist['min_dist'] = dists
+        min_dist['closest_doc'] = closest
+        print('finished writing pandas')
+        min_dist.to_pickle('data/{year}_distances.pd'.format(year=year))
+        print('finished pickling')
 
-        res[year] = min_dist
-    return res
+
+def compute_dists(X_prev):
+    dists = cdist(X_prev, X_cur, metric='cityblock')
+    min_dists = dists.min(axis=1)
+    closest_ixs = X_cur.index[dists.argmin(axis=1)]
+    return (min_dists, closest_ixs)
 
 
 def obtain_min_distances(X_topic, years):
@@ -47,7 +64,5 @@ if __name__ == '__main__':
     X_multopic = pd.DataFrame(X_multopic, index=ids_corpus)
     X_multopic['year'] = dates_corpus
     print('compute distances')
-    min_dists = obtain_min_dist_split(X_multopic, set(dates_corpus), max_mem=10**9)
-    print('dump results')
-    pickle.dump(min_dists, open('data/min_dists.pkl', 'wb'))
+    obtain_min_dist_split(X_multopic, set(dates_corpus), max_mem=10**7)
     print('finished.')
