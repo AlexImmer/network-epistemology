cc9f4921cdfae7e945fec27ce0d679ee954190d1
Alex Immer <alexndr.immer@gmail.com>
2018-04-20 15:50:51 +0200

    Extend loading and init data analysis notebook

--- loading.py
+++ loading.py
@@ -1,13 +1,13 @@
 import json
-import pickle
 import numpy as np
 import pandas as pd
 
-
+data_dir = 'data/'
 corpus_files = ['publication_data/dblp-ref-0.json',
                 'publication_data/dblp-ref-1.json',
                 'publication_data/dblp-ref-2.json',
                 'publication_data/dblp-ref-3.json']
+unconnected = 'weird_publications.txt'
 
 
 def load_file(dblp_file):
@@ -35,10 +35,45 @@ def load_corpus(files=None):
 
 
 def load_doc_topics(file=None):
-    file = file or 'data/X_multopic.npy'
+    file = file or data_dir + 'X_multopic.npy'
     return np.load(file)
 
 
-def load_delta(year):
-    file = 'data/{year}_distances.pd'.format(year=year)
-    return pd.read_pickle(file)
+def load_concept_distances(year, keep_closeest_doc=False):
+    file = data_dir + '{year}_distances.pd'.format(year=year)
+    df = pd.read_pickle(file)
+    if not keep_closeest_doc:
+        del df['closest_doc']
+        df.columns = ['concept_distance']
+    df.index.name = 'pub_id'
+    return df
+
+
+def load_transformation_distances(year):
+    file = data_dir + 'transformation{year}.csv'.format(year=year)
+    df = pd.read_csv(file, index_col=0)
+    df.columns = ['transformation_distance']
+    return df
+
+
+def load_tradition_distances(year):
+    file = data_dir + 'tradition{year}.csv'.format(year=year)
+    df = pd.read_csv(file, index_col=0)
+    df.columns = ['tradition_distance']
+    return df
+
+
+def load_distances(year):
+    # TODO: remove broken papers
+    conc = load_concept_distances(year)
+    trans = load_transformation_distances(year)
+    trad = load_tradition_distances(year)
+    # conc contains all the right indices
+    res = conc.merge(trans, how='left', left_index=True, right_index=True)
+    res = res.merge(trad, how='left', left_index=True, right_index=True)
+    return res
+
+
+def load_disconnected_publications():
+    pub_ids = pd.read_csv(data_dir + unconnected, index_col=0, names=[])
+    return pub_ids
