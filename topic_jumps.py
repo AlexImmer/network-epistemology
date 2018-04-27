4631510681180bb6f27b485c41aa69dc5edad4cc
Alex Immer <alexndr.immer@gmail.com>
2018-04-27 20:08:43 +0200

    MP minimum search

--- topic_jumps.py
+++ topic_jumps.py
@@ -11,6 +11,9 @@ k = 100  # n topics
 
 
 def obtain_jump_edge_distances(X_topic, years, max_mem=10**7, tradition=True):
+    global col
+    global g_dists
+    global X_cur
     col = ('tradition' if tradition else 'transformation') + '_distance'
     for year in tqdm(sorted(list(years))[1:]):
         try:
@@ -22,31 +25,27 @@ def obtain_jump_edge_distances(X_topic, years, max_mem=10**7, tradition=True):
         except:
             pass
         cols = np.arange(0, k)
-        dists = load_graph_distances(year)
+        g_dists = load_graph_distances(year)
         # extend dists by current year pubs (dist = 0)
         current = X_topic.loc[(X_topic['year'] == year)].index
         current = pd.DataFrame(0, columns=[col], index=current)
-        dists = dists.append(current)
-        disconnected = dists[dists[col].isna()].index
-        connected = dists[dists[col].notna()].index
+        g_dists = g_dists.append(current)
+        disconnected = g_dists[g_dists[col].isna()].index
+        connected = g_dists[g_dists[col].notna()].index
         X_prev = X_topic.loc[(X_topic['year'] < year), cols]
         X_prev = X_prev.loc[disconnected]
-        global X_cur
         X_cur = X_topic.loc[(X_topic['year'] <= year), cols]
         X_cur = X_cur.loc[connected]
         ix_stepsize = int(max_mem / len(X_cur))
         X_prevs = [X_prev.iloc[i: i+ix_stepsize] for i in range(0, len(X_prev), ix_stepsize)]
-        with Pool(processes=18) as pool:
+        with Pool(processes=26) as pool:
             res_list = pool.map(find_edges, X_prevs)
-        map = {}
+        ixs, dists = [], []
         for res in res_list:
-            for s, e in zip(res[0], res[1]):
-                if map.get(s):
-                    map[s].append(e)
-                else:
-                    map[s] = [e]
-        res = pd.DataFrame(index=list(map.keys()))
-        res['topic_' + col] = res.index.map(lambda x: dists.loc[map[x], col].min())
+            ixs.extend(res[0])
+            dists.extend(res[1])
+        res = pd.DataFrame(index=ixs)
+        res['topic_' + col] = dists
         case = 'tradition' if tradition else 'transformation'
         file = data_dir + 'jump_{case}{year}.csv'.format(year=year, case=case)
         res.to_csv(file, index_label='pub_id')
@@ -58,7 +57,13 @@ def find_edges(X_prev):
     dists = (cdist(X_prev >= 1 / k, X_cur.values >= 1 / k, metric='russellrao') * k) - k \
             + np.sum(X_prev >= 1 / k, axis=1)[:, np.newaxis]
     starts, ends = np.where(dists == 0)
-    return list(X_prev.index[starts]), list(X_cur.index[ends])
+    map = {}
+    for s, e in zip(X_prev.index[starts], X_cur.index[ends]):
+        if map.get(s):
+            map[s].append(e)
+        else:
+            map[s] = [e]
+    return list(map.keys()), [g_dists.loc[map[x], col].min() for x in map]
 
 
 if __name__ == '__main__':
