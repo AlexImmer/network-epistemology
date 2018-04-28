eff3fdc6ff45f3bc3b8d0ab97b5aaf474a46cb86
Alex Immer <alexndr.immer@gmail.com>
2018-04-28 09:48:11 +0200

    Use numpy indexing all the way through

--- topic_jumps.py
+++ topic_jumps.py
@@ -28,24 +28,21 @@ def obtain_jump_edge_distances(X_topic, years, max_mem=10**7, tradition=True):
         g_dists = load_graph_distances(year)
         # extend dists by current year pubs (dist = 0)
         current = X_topic.loc[(X_topic['year'] == year)].index
-        current = pd.DataFrame(0, columns=[col], index=current)
+        current = pd.DataFrame(0., columns=[col], index=current)
         g_dists = g_dists.append(current)
         disconnected = g_dists[g_dists[col].isna()].index
         connected = g_dists[g_dists[col].notna()].index
+        g_dists = g_dists.loc[connected]
         X_prev = X_topic.loc[(X_topic['year'] < year), cols]
         X_prev = X_prev.loc[disconnected]
         X_cur = X_topic.loc[(X_topic['year'] <= year), cols]
         X_cur = X_cur.loc[connected]
         ix_stepsize = int(max_mem / len(X_cur))
         X_prevs = [X_prev.iloc[i: i+ix_stepsize] for i in range(0, len(X_prev), ix_stepsize)]
-        with Pool(processes=26) as pool:
+        with Pool(processes=32) as pool:
             res_list = pool.map(find_edges, X_prevs)
-        ixs, dists = [], []
-        for res in res_list:
-            ixs.extend(res[0])
-            dists.extend(res[1])
-        res = pd.DataFrame(index=ixs)
-        res['topic_' + col] = dists
+        res = pd.concat(res_list)
+        res.columns = ['topic_' + col]
         case = 'tradition' if tradition else 'transformation'
         file = data_dir + 'jump_{case}{year}.csv'.format(year=year, case=case)
         res.to_csv(file, index_label='pub_id')
@@ -57,13 +54,10 @@ def find_edges(X_prev):
     dists = (cdist(X_prev >= 1 / k, X_cur.values >= 1 / k, metric='russellrao') * k) - k \
             + np.sum(X_prev >= 1 / k, axis=1)[:, np.newaxis]
     starts, ends = np.where(dists == 0)
-    map = {}
-    for s, e in zip(X_prev.index[starts], X_cur.index[ends]):
-        if map.get(s):
-            map[s].append(e)
-        else:
-            map[s] = [e]
-    return list(map.keys()), [g_dists.loc[map[x], col].min() for x in map]
+    df = pd.DataFrame()
+    df['pub_id'] = X_prev.index[starts]
+    df['dists'] = np.array(g_dists[col])[ends]
+    return df.groupby('pub_id').min()
 
 
 if __name__ == '__main__':
@@ -80,7 +74,7 @@ if __name__ == '__main__':
     del dates_corpus
     del ids_corpus
     print('Tradition')
-    obtain_jump_edge_distances(X_multopic, years, max_mem=10**7, tradition=True)
+    obtain_jump_edge_distances(X_multopic, years, max_mem=10**8, tradition=True)
     print('Transformation')
-    obtain_jump_edge_distances(X_multopic, years, max_mem=10**7, tradition=False)
+    obtain_jump_edge_distances(X_multopic, years, max_mem=10**8, tradition=False)
     print('finished.')
